87c124f7ba518c1c3399d5ddc6f5bee03791fc46
feixueSuifeng <936029669@qq.com>
2017-05-01 10:51:12 +0800

    再来测试一下分支合并到主分支

--- aaa/src/com/my/test/Test2.java
+++ aaa/src/com/my/test/Test2.java
@@ -3,6 +3,6 @@ package com.my.test;
 public class Test2 {
 	public static void main(String[] args) {
 		System.out.println("测试分支");
-		System.out.println("测试分支合并");
+		System.out.println("测试分支合并11");
 	}
 }
